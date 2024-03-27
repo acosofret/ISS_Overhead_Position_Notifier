0207827e6e42be41b21087c2edab5ee404dbc5e2
acosofret <acosofretcwst@gmail.com>
2024-03-27 21:01:29 +0000

    The program is not functional yet. Work in progress...

---
+++ main.py
@@ -0,0 +1,38 @@
+import requests
+from datetime import datetime
+
+# First, we set the Latitude & Longitude for our location as variables:
+# You can find the exact Latitude & Longitude for your location on this website: https://www.latlong.net/
+MY_LAT = 51.400459 # Your latitude
+MY_LONG = -1.321850 # Your longitude
+
+# Then we request the ISS position from the respective endpoint:
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+
+#Your position is within +5 or -5 degrees of the ISS position.
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+time_now = datetime.now()
+
+#If the ISS is close to my current position
+# and it is currently dark
+# Then send me an email to tell me to look up.
+# BONUS: run the code every 60 seconds.
+
+
+
